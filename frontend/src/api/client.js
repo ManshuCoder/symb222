d1ac12882a6851844b001d951753acba58bb7790
ManshuCoder <htamarkandi@gmail.com>
2026-02-20 01:19:19 +0530

    fifth commit

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,21 +1,21 @@
-import axios from "axios";
+import axios from "axios" ;
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api" ,
   timeout: 10000
 });
 
 api.interceptors.response.use(
-  (response) => response,
+  (response) => response ,
   (error) => {
     const message =
       error.response?.data?.message ||
       error.message ||
-      "Something went wrong. Please try again.";
-    return Promise.reject(new Error(message));
+      "Something went wrong. Please try again." ;
+    return Promise.reject(new Error(message)) ;
   }
-);
+) ;
 
-export default api;
+export default api ;
 
 
